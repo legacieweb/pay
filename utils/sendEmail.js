e86ccf40bbb2c62f8b38f2decf55ed09ebdd8031
Bonface <iyonicorp@gmail.com>
2025-06-15 22:56:41 +0300

    L

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -11,7 +11,7 @@ const sendEmail = async ({ to, subject, html, text }) => {
     });
 
     const mailOptions = {
-      from: `"IyonicPay💸" <${process.env.EMAIL_USER}>`,
+      from: `"IyonicPay" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text: text || '', // fallback for clients that don't support HTML
